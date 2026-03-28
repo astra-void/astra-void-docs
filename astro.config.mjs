68b061218e52f5cd1980b65027d037c63ef2da4d
astra-void <dev@astra-void.xyz>
2026-03-28 18:35:41 +0900

    feat: init shadcn/ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
-import mdx from "@astrojs/mdx";
+﻿import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
@@ -7,7 +8,7 @@ const docsSiteUrl = process.env.DOCS_SITE_URL;
 export default defineConfig({
   base: "/",
   ...(docsSiteUrl ? { site: docsSiteUrl } : {}),
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
   vite: {
     plugins: [tailwindcss()],
   },
